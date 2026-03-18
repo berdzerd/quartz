f0974fd8ebd727b7abc94dad3e9f7715ab2cc6fb
berdzerd <Samuel.Lindley95@gmail.com>
2026-03-19 01:03:27 +0900

    Quartz sync: Mar 19, 2026, 1:03 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Jetbrains Mono",
-        body: "Jetbrains Mono",
+        header: "JetBrains Mono",
+        body: "JetBrains Mono",
         code: "Nova Mono",
       },
 
