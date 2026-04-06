114a640432d7983ec20a872125302c8f73e2307a
berdzerd <Samuel.Lindley95@gmail.com>
2026-04-07 00:16:53 +0900

    Quartz sync: Apr 7, 2026, 12:16 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
       <p>
           Berdzerd's Garden © {year}, licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a> <img
-          src="/quartz/qurtz/static/cc.svg"
+          src="/quartz/static/cc.svg"
           alt="Creative Commons CC Icon"
           width="20"
           height="20"
@@ -25,7 +25,7 @@ export default ((opts?: Options) => {
           }}
           />
           <img
-          src="quartz/quartz/static/by.svg"
+          src="/quartz/static/by.svg"
           alt="Creative Commons BY Icon"
           width="20"
           height="20"
