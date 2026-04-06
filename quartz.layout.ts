2b3de007b497dc07052a4a0b66469660632141d4
berdzerd <Samuel.Lindley95@gmail.com>
2026-04-06 23:58:47 +0900

    Quartz sync: Apr 6, 2026, 11:58 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-    //  GitHub: "https://github.com/berdzerd/quartz",
+      GitHub: "https://github.com/berdzerd/quartz",
     },
   }),
 }
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,26 +15,26 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
       <p>
            Berdzerd's Garden © {year}, licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a> <img
-+          src="/static/cc.svg"
-+          alt="Creative Commons CC Icon"
-+          width="20"
-+          height="20"
-+          style={{
-+            verticalAlign: "middle", // Aligns with text baseline
-+            display: "inline-block",     // Keeps the icon inline
-+          }}
-+          />
-+          <img
-+          src="/static/by.svg"
-+          alt="Creative Commons BY Icon"
-+          width="20"
-+          height="20"
-+          style={{
-+            verticalAlign: "middle", // Aligns with text baseline
-+            display: "inline-block",     // Keeps the icon inline
-+          }}
-+          />
-+         </p>
+          src="/static/cc.svg"
+          alt="Creative Commons CC Icon"
+          width="20"
+          height="20"
+          style={{
+            verticalAlign: "middle", // Aligns with text baseline
+            display: "inline-block",     // Keeps the icon inline
+          }}
+          />
+          <img
+          src="/static/by.svg"
+          alt="Creative Commons BY Icon"
+          width="20"
+          height="20"
+          style={{
+            verticalAlign: "middle", // Aligns with text baseline
+            display: "inline-block",     // Keeps the icon inline
+          }}
+          />
+         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
